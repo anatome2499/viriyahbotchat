4f4ae792ada013154c932a9279beabcf07efc238
anatome2499 <anatome2499@hotmail.com>
2017-07-06 15:43:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "hello viriyah"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
