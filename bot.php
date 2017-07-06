59f8a3d11ed944f5a06129390e1a2473089e0553
anatome2499 <anatome2499@hotmail.com>
2017-07-06 16:50:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = 'd9RBQdOTTADhGfT7TBl000WUsIjVfOkF9nlWPoI0FRDUtr5Xkk7z98cJkrXNMM5mVx0t4b9anVLsQqhQ58uZ4YNg2qvwgmNG1XFxUzi+0o4sqiUYD+9wtujtE4wa2Z4QzAuVltWNRvxkux+fqvCRKgdB04t89/1O/w1cDnyilFU=';
 
+// Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
@@ -17,9 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				"type": "sticker",
-  				"packageId": "1",
-  				"stickerId": "1"
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
