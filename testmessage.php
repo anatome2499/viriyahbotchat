d547a69fcb33b23583029b5d03a1da3c6e5672b4
anatome2499 <anatome2499@hotmail.com>
2017-07-06 16:17:56 +0700

    Update testmessage.php

--- testmessage.php
+++ testmessage.php
@@ -1,22 +1,55 @@
 <? {
+{
   "type": "template",
-  "altText": "this is a confirm template",
+  "altText": "this is a carousel template",
   "template": {
-      "type": "confirm",
-      "text": "Are you sure?",
-      "actions": [
+      "type": "carousel",
+      "columns": [
           {
-            "type": "message",
-            "label": "Yes",
-            "text": "yes"
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
+                }
+            ]
           },
           {
-            "type": "message",
-            "label": "No",
-            "text": "no"
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
+                }
+            ]
           }
       ]
   }
 }
-echo "test111";
 ?>
