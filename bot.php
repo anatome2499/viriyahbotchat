5d3d1ed9655154f04694488fd8f9a49108ef140f
anatome2499 <anatome2499@hotmail.com>
2017-07-06 16:37:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,11 +18,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => "hello viriyah",
 				'type' => 'sticker',
-				'packageId'=>["1"],
-				'stickerId'=>["138"]
+				'packageId'=>"1",
+				'stickerId'=>"138"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
