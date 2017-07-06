f3af2bf9a14ec49c9a3b3f4139f51bf4e8197b7d
anatome2499 <anatome2499@hotmail.com>
2017-07-06 16:43:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,11 +18,19 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				"type": "template",
+				
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => {
+  "type": "template",
   "altText": "this is a confirm template",
   "template": {
       "type": "confirm",
-      "text": "วิริยะรึเปล่า?",
+      "text": "Are you sure?",
       "actions": [
           {
             "type": "message",
@@ -36,13 +44,7 @@ if (!is_null($events['events'])) {
           }
       ]
   }
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
+},
 				
 				
 			];
