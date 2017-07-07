a3a22a28a6d5d41f442c2240589f318a359f127b
anatome2499 <anatome2499@hotmail.com>
2017-07-07 09:29:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,3 @@
-<?php
-$access_token = 'd9RBQdOTTADhGfT7TBl000WUsIjVfOkF9nlWPoI0FRDUtr5Xkk7z98cJkrXNMM5mVx0t4b9anVLsQqhQ58uZ4YNg2qvwgmNG1XFxUzi+0o4sqiUYD+9wtujtE4wa2Z4QzAuVltWNRvxkux+fqvCRKgdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,30 +13,10 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
-			//buildaction
-			$action1=['type'=> 'message',
-            'label'=> 'Yes',
-            'text'=> 'yes'];
-
-            $action2=['type'=> 'message',
-            'label'=> 'No',
-            'text'=> 'no'];
-
-			//build template&action
-			$action ='actions'=> [
-          $action1,
-          $action2];
-			$template ='template'=> [
-      		'type'=> 'confirm',
-      		'text'=> 'Are you sure?',
-      		$action
-  		];
-
 			// Build message to reply back
 			$messages = [
-				'type'=> 'template',
-  				'altText'=> 'this is a confirm template',
-  				$template
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -65,4 +42,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-			
