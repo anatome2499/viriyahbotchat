75975035c4344084989914e3042d5059393376dd
anatome2499 <anatome2499@hotmail.com>
2017-07-07 09:29:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,7 @@
+<?php
+$access_token = 'd9RBQdOTTADhGfT7TBl000WUsIjVfOkF9nlWPoI0FRDUtr5Xkk7z98cJkrXNMM5mVx0t4b9anVLsQqhQ58uZ4YNg2qvwgmNG1XFxUzi+0o4sqiUYD+9wtujtE4wa2Z4QzAuVltWNRvxkux+fqvCRKgdB04t89/1O/w1cDnyilFU=';
+
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
