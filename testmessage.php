4b28731f085c052fe16e382803ea983429928bba
anatome2499 <anatome2499@hotmail.com>
2017-07-07 15:13:12 +0700

    test template message 15.13

--- testmessage.php
+++ testmessage.php
@@ -5,8 +5,8 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
 $logger = new Logger('LineBot');
 $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient("d9RBQdOTTADhGfT7TBl000WUsIjVfOkF9nlWPoI0FRDUtr5Xkk7z98cJkrXNMM5mVx0t4b9anVLsQqhQ58uZ4YNg2qvwgmNG1XFxUzi+0o4sqiUYD+9wtujtE4wa2Z4QzAuVltWNRvxkux+fqvCRKgdB04t89/1O/w1cDnyilFU=");
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => "9e8ce9a241ec4a2f43b185e421d6c637"]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('d9RBQdOTTADhGfT7TBl000WUsIjVfOkF9nlWPoI0FRDUtr5Xkk7z98cJkrXNMM5mVx0t4b9anVLsQqhQ58uZ4YNg2qvwgmNG1XFxUzi+0o4sqiUYD+9wtujtE4wa2Z4QzAuVltWNRvxkux+fqvCRKgdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '9e8ce9a241ec4a2f43b185e421d6c637']);
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
@@ -86,7 +86,7 @@ foreach ($events as $event) {
     }
     $response = $bot->replyMessage($event->getReplyToken(), $outputText);
   }
-} 
+}  
 
-echo "15.08"; 
+echo "15.12"; 
 ?>
