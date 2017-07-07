183fa85b9f45343324859c35e93a85eb991acad6
anatome2499 <anatome2499@hotmail.com>
2017-07-07 11:23:48 +0700

    test template message 11.23

--- bot.php
+++ bot.php
@@ -35,13 +35,15 @@ if (!is_null($events['events'])) {
             $template=[
             			"type"=> "confirm",
       					"text"=> "Are you sure?",
-      					"actions"=>$action];
+      					"actions"=>[$action]
+      					];
 
 			// Build message to reply back
 			$messages = [
 							"type"=> "template",
   							"altText"=> "this is a confirm template",
-  							"template"=> $template];
+  							"template"=> [$template]
+  							];
 ];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
