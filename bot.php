e2adbd1832f77acfdd2696c695d838100e9d570d
anatome2499 <anatome2499@hotmail.com>
2017-07-07 13:24:36 +0700

    test template message 13.24

--- bot.php
+++ bot.php
@@ -19,26 +19,44 @@ if (!is_null($events['events'])) {
 			
 
 			// Build message to reply back
-			$messages = json_decode('{
-  '.'"type"'.':'.' "template"'.',
-  '.'"altText"'.':'.' "this is a confirm template"'.','.
-  '"template"'.':'. '{
-      '.'"type"'.':'.' "confirm"'.','.
-      '"text"'.':'.' "Are you sure?"'.','.
-      '"actions"'.':'.' [
-          {
-            '.'"type"'.':'.' "message"'.','.
-            '"label"'.':'.' "Yes"'.','.
-            '"text"'.':'.' "yes"'.'
-          },
-          {
-            '.'"type"'.':'. '"message"'.','.
-            '"label"'.':'. '"No"'.','.
-            '"text"'.':'. '"no"'.
-          '}
-      ]
-  }
-}'.'")';
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+		else if($event['type']=='message'&&$event['message']['type']=='sticker'){
+			//get sticker sent
+			$sticker=$event['message']['packageId']['stickerId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'sticker',
+				'sticker' => $sticker
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -62,7 +80,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "13.11";
+echo "13.24";
 
 
 ?>
