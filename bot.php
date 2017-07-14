c37a44f29f0114ec4eb853fed5779bca546d3b44
anatome2499 <anatome2499@hotmail.com>
2017-07-14 10:51:33 +0700

    10.52

--- bot.php
+++ bot.php
@@ -43,12 +43,12 @@ if (!is_null($events['events'])) {
 				];
 			}
 			//else if($text=="button template"){
-				//$messages[
-				//"type"=>"template",
-				//"altText"=>"this is a button template",
-				//"template"=>array('type' =>"buttons" ,"thumbnailImageUrl"=> "https://viriyahbotchat.herokuapp.com/2017-07-14_9-08-48.png","title"=>"Menu","text"=>"Please select","actions"=>array(array('type' =>"postback" ,"label"=>"Buy","text"=>"buy" ),array('type' =>"message" ,"label"=>"Add to cart","text"=>"add to cart" ),array('type' =>"uri" ,"label"=>"View detail","uri"=>"http://www.viriyah.co.th/th/product.php" ) ))
-				//];
-			//}
+				$messages[
+				'type'=>"template",
+				'altText'=>"this is a button template",
+				'template'=>array('type' =>"buttons" ,'thumbnailImageUrl'=> "https://viriyahbotchat.herokuapp.com/2017-07-14_9-08-48.png",'title'=>"Menu",'text'=>"Please select",'actions'=>array(array('type' =>"postback" ,'label'=>"Buy",'text'=>"buy" ),array('type' =>"message" ,'label'=>"Add to cart",'text'=>"add to cart" ),array('type' =>"uri" ,'label'=>"View detail",'uri'=>"http://www.viriyah.co.th/th/product.php" ) ))
+				];
+			}
 			else{
 				// Build message to reply back
 			$messages = [
@@ -165,7 +165,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "10.45";
+echo "10.52";
 
 
 ?>
