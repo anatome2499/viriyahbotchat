c59143ed42b59baeff8bdfea8925610853de0dd8
anatome2499 <anatome2499@hotmail.com>
2017-07-14 11:03:51 +0700

    11.04

--- bot.php
+++ bot.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 				$messages[
 				'type'=>'template',
 				'altText'=>"this is a buttons template",
-				'template'=>array('type' =>"buttons" ,'thumbnailImageUrl'=>"https://viriyahbotchat.herokuapp.com/2017-07-14_9-08-48.png",'title'=>"Menu",'text'=>"Please select",'actions'=>array(array('type' =>"postback" ,'label'=>"Buy",'data'=>"action=buy&itemid=123" ),array('type' =>"postback" ,'label'=>"Add to cart",'data'=>"action=add&itemid=123" ),array('type' =>"uri" ,'label'=> "View detail",'uri'=>"http://example.com/page/123") ) )
+				'template'=>array('type' =>"buttons" ,'thumbnailImageUrl'=>"https://example.com/bot/images/image.jpg",'title'=>"Menu",'text'=>"Please select",'actions'=>array(array('type' =>"postback" ,'label'=>"Buy",'data'=>"action=buy&itemid=123" ),array('type' =>"postback" ,'label'=>"Add to cart",'data'=>"action=add&itemid=123" ),array('type' =>"uri" ,'label'=> "View detail",'uri'=>"http://example.com/page/123") ) )
 				];
 			}
 			else{
@@ -165,7 +165,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "11.02";
+echo "11.04";
 
 
 ?>
