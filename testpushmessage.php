e803de11ea452e95f4dc8360c9f24db0bc2baaf9
anatome2499 <anatome2499@hotmail.com>
2020-03-06 14:08:17 +0700

    Update testpushmessage.php

--- testpushmessage.php
+++ testpushmessage.php
@@ -2,47 +2,23 @@
 $access_token = 'zwU1HU8d9UeU1ADrGBIn3miov2UMLAvwvSf2KKdODHsJQSmq+J46aFS7icqfwjKJtykwCzPt6GKm60SuxS/zHz8PAasauZHd/C5B0ZY+9Mef8UR3tOaSJlHMbV8l1D1jsgRLC0bolKI7h8eUhsYN0QdB04t89/1O/w1cDnyilFU=';
 $userId="U123a75ddc358905d48b16cf4b20c3acf";
 $messages=[
-				'type'=>"template",
-				'altText'=>"this is a carousel template",
+				'type'=>'template',
+				'altText'=>"this is a confirm template",
 				'template'=>array(
-								'type' =>"carousel" ,
-								'columns'=>array(
-												array(
-													'thumbnailImageUrl' =>"https://daily.rabbitstatic.com/wp-content/uploads/2013/11/viriyah.jpg" ,
-												 	'title'=>"this is menu",
-												 	'text'=>"description",
-												 	'actions'=>array(
-												 					array(
-												 						'type' => "message",
-												 						'label'=>"Yes",
-												 						'text'=>"yes" 
-												 						),
-												 					array(
-												 						'type' =>"uri" ,
-												 						'label'=>"detail",
-												 						'uri'=>"https://www.google.co.th" 
-												 						)
-												 					)
-												 	)  ,
-												array(
-												 	'thumbnailImageUrl' =>"https://daily.rabbitstatic.com/wp-content/uploads/2013/11/viriyah.jpg" ,
-												 	'title'=>"this is menu",
-												 	'text'=>"description",
-												 	'actions'=>array(
-												 					array(
-												 						'type' => "message",
-												 						'label'=>"Yes",
-												 						'text'=>"yes" 
-												 						),
-												 					array(
-												 						'type' =>"uri" ,
-												 						'label'=>"detail",
-												 						'uri'=>"http://www.viriyah.co.th/th/product.php" 
-												 						)
-												 					) 
-												 	)
-												) 
-								)
+					'type' =>"confirm" ,
+					'text'=>"Are you sure?",
+					'actions'=>array(
+									array(
+										'type' =>'message' ,
+										'label'=>"Yes",
+										'text'=>'yes'),
+									array(
+										'type' =>'message' ,
+										'label'=>"No",
+										'text'=>'no'
+										) 
+									) 
+					)
 				];
 // Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
